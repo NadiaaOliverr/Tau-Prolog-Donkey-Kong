c082df1906f8a9e03abf0f6920093118b78c507b
Genilton <genilton2528>
2019-11-14 17:59:45 -0300

    Gerando numero aleatorio

--- Donkey_Kong/main.js
+++ Donkey_Kong/main.js
@@ -9,10 +9,9 @@ let positionHammer = [];
 function drawScreen(){
     for(let i = 0; i < 5; i++){
         let row = document.createElement('tr');
-        row.setAttribute('id', i);
         for(let j = 0; j < 10; j++){
             let column = document.createElement('td');
-            column.setAttribute('id', j);                
+            column.setAttribute('id', `${i} ${j}`);
             column.setAttribute('class', 'tabuleiro');
             row.append(column);
         }    
@@ -21,7 +20,19 @@ function drawScreen(){
 }
 
 function generateField(){
-
+    let row = parseInt( Math.random()*5 );
+    let column = parseInt( Math.random()*10 );
+    console.log(row, column);
+    let box = document.getElementById(`${row} ${column}`);
+    let img = document.createElement('img');
+    img.setAttribute('src', 'mario.png');
+    img.setAttribute('class', 'imgBox');
+    box.style.width = 0;
+    box.style.height = 0;
+    box.setAttribute('class', '');
+    box.append(img);
+    box.style.border = '4px solid #fff';
 }
 
 drawScreen();
+generateField();
